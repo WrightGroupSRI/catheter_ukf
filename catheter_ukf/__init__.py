0aa8b94c1c37d1c9d035d23c204039868081d3b3
Sebastian Ferguson <ferguson@sri.utoronto.ca>
2019-09-10 11:02:12 -0400

    Fixup setup.py so that it installs requirements

--- catheter_ukf/__init__.py
+++ catheter_ukf/__init__.py
@@ -2,7 +2,7 @@
 tracking problem.
 
 Most of the stuffs found within are "implementation details". For users
-the most relevant things are exported here: `UKF` and `NoFilter`.
+the most relevant things are exported here: `UKF`.
 """
 
 from .ukf import UKF  # This is the UKF for catheter tracking
